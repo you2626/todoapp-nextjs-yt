d078a199f22eaf8d0feace92401f1979e1902730
KURUMI <k.1au12um1@gmail.com>
2024-09-20 00:26:14 +0900

    Add AddTask,TodoList

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const TodoList = () => {
   return (
-    <div>TodoList</div>
+    <ul className="space-y-3">
+        <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rouded shadow">
+            <span>散歩</span>
+            <div>
+                <button className="text-green-500 mr-3">edit</button>
+                <button className="text-red-500">Delete</button>
+            </div>
+        </li>
+    </ul>
   )
 }
 
-export default TodoList
+export default TodoList;
